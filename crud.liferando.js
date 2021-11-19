6c3f5974223c9d03e0be9ce71d0551ed4f163c2a
ErwinFeld <erwinmailkonto@gmail.com>
2021-11-19 13:38:23 +0100

    added clean approach for items

---
+++ crud.liferando.js
@@ -0,0 +1,13 @@
+
+const priceList = [
+  { id: 0, item: "cheese", price: 2 },
+  { id: 1, item: "meat", price: 2.5 },
+  { id: 2, item: "fish", price: 4 },
+
+];
+
+
+const shoppingCard = [];
+
+
+
