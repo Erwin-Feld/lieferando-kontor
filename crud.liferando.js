eebd4ecae8dddfe1bf08e31cf5712436a8d8757c
ErwinFeld <erwinmailkonto@gmail.com>
2021-11-19 14:43:47 +0100

    added correct updating value if added product

--- crud.liferando.js
+++ crud.liferando.js
@@ -1,36 +1,60 @@
-
-const priceList = [
+const itemsDb = [
   { id: 0, item: "cheese", price: 2 },
   { id: 1, item: "meat", price: 2.5 },
   { id: 2, item: "fish", price: 4 },
-
 ];
-
 
 const shoppingCard = [];
 
+function render(itemsDb, shoppingCard) {
+  const parentContainer = document.querySelector(".container");
 
+  itemsDb.forEach((element, i) => {
+    const buyBtn = document.createElement("button");
 
+    //Add how to pase parameters and bind to addItems?
+    buyBtn.addEventListener("click", addToShoppingCard);
 
-function render(priceList) {
-    const parentContainer = document.querySelector(".container");
-  
-    priceList.forEach((element, i) => {
-      const buyBtn = document.createElement("button");
-      
-      //Add how to pase parameters and bind to addItems?
-    //   buyBtn.addEventListener("click", addItem);
+    buyBtn.setAttribute("id", i);
+    buyBtn.textContent = itemsDb[i].item;
+    parentContainer.appendChild(buyBtn);
+  });
+}
 
+function addToShoppingCard() {
+   
+  const itemIndexId = parseInt(this.id);
 
-      buyBtn.setAttribute("id", i);
-      buyBtn.textContent = priceList[i].item;
-      parentContainer.appendChild(buyBtn);
-    });
+  if (shoppingCard.some((item) => item.id === itemIndexId)) {
+    //   const indexNR = shoppingCard.some((item) => item.id === itemIndexId
+
+      const index = shoppingCard.findIndex(item => item.id === itemIndexId);
+    //   FIXME current Idex error 
+        updateItem(shoppingCard, itemsDb, itemIndexId)
+
+        console.log(shoppingCard[1])
+    // update
+  } else {
+    // add
+    shoppingCard.push(itemsDb[itemIndexId]);
   }
-  
+
+}
+
+function updateShoppingCard() {}
 
 
-  function addToShoppingCard(){
-    const itemIndexId = parseInt(this.id);
+
+function updateItem(shoppingCard, itemsDb, itemIndexId) {
+   
+    return (shoppingCard[itemIndexId] = {
+     
+      ...shoppingCard[itemIndexId],
+        
+    //   added to the current value the ++ value
+        price:  (shoppingCard[itemIndexId].price+itemsDb[itemIndexId].price) 
     
+    });
+
+   
   }
