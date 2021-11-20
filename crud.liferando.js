50c86fb515f7f757b0fdf7b919331e2dfde85b78
ErwinFeld <erwinmailkonto@gmail.com>
2021-11-20 11:20:01 +0100

    bug fix  correctly target delete container

--- crud.liferando.js
+++ crud.liferando.js
@@ -25,30 +25,66 @@ function renderItemsDb(itemsDb, shoppingCard) {
   });
 }
 
-
-function renderShoppingCard(shoppingCard){
-    // delete check 
-    if( document.querySelector("card-container")){
-        const cardContainer = document.querySelector("card-container");
-        cardContainer.innerHTML = "";
-    }
-
-    const container = document.createElement("div");
-    container.setAttribute("class", "card-container");
+// FIXME
+// add correctly renderShoppingCard
+function renderShoppingCard(shoppingCard) {
+  // delete check
+  const cardContainer = document.querySelector(".card-container");
+ 
+  if (document.body.contains(cardContainer)) {
   
+    const cardContainer = document.querySelector(".card-container");
+    cardContainer.innerHTML = "";
+   
     shoppingCard.forEach((element, i) => {
+      // const cardContainer = document.querySelector(".card-container");
       const removeBtn = document.createElement("button");
   
       //Add how to pase parameters and bind to addItems?
       removeBtn.addEventListener("click", delOfShoppingCard);
   
-      removeBtn.setAttribute("id", i);
+      removeBtn.setAttribute("id", i + 0.1);
       removeBtn.textContent = shoppingCard[i].item + " " + shoppingCard[i].price;
-      container.appendChild(removeBtn);
-  
-      const parentContainer = document.querySelector(".container");
-      parentContainer.appendChild(container);
+      cardContainer.appendChild(removeBtn);
     });
+    
+  } else {
+  
+    const cardContainer = document.createElement("div");
+    cardContainer.setAttribute("class", "card-container");
+    const parentContainer = document.querySelector(".container");
+    parentContainer.appendChild(cardContainer);
+
+    shoppingCard.forEach((element, i) => {
+      // const cardContainer = document.querySelector(".card-container");
+      const removeBtn = document.createElement("button");
+  
+      //Add how to pase parameters and bind to addItems?
+      removeBtn.addEventListener("click", delOfShoppingCard);
+  
+      removeBtn.setAttribute("id", i + 0.1);
+      removeBtn.textContent = shoppingCard[i].item + " " + shoppingCard[i].price;
+      cardContainer.appendChild(removeBtn);
+    });
+    
+  }
+  
+
+}
+
+function createShoppingItems(shoppingCard){
+  
+  shoppingCard.forEach((element, i) => {
+    // const cardContainer = document.querySelector(".card-container");
+    const removeBtn = document.createElement("button");
+
+    //Add how to pase parameters and bind to addItems?
+    removeBtn.addEventListener("click", delOfShoppingCard);
+
+    removeBtn.setAttribute("id", i + 0.1);
+    removeBtn.textContent = shoppingCard[i].item + " " + shoppingCard[i].price;
+    cardContainer.appendChild(removeBtn);
+  });
 
 }
 
@@ -59,63 +95,67 @@ function addToShoppingCard() {
   /* if item already appended */
   if (shoppingCard.some((item) => item.id === itemIndexId)) {
     updateShoppingCard(shoppingCard, itemsDb, itemIndexId);
-  
-//    renderShoppingCard(shoppingCard);
+
+    //    renderShoppingCard(shoppingCard);
     // render basket
+    renderShoppingCard(shoppingCard);
   } else {
-     
     /* append item */
     shoppingCard.push(itemsDb[itemIndexId]);
     // renderShoppingCard(shoppingCard);
     // render basket
-   
+    renderShoppingCard(shoppingCard);
   }
-console.log(shoppingCard)
-  
+  console.log(shoppingCard);
 }
 
 /**
  *
- * @param { Array } objectContainer - looped over objectContainer
+
  * @param { Number } itemIndex current Item index
+ * @param { Array } objectContainer - looped over objectContainer
  */
 function updateShoppingCard(shoppingCard, itemsDb, dbItemIndexId) {
+  const currItemIndxPos = shoppingCard.findIndex(
+    (item) => item.id === dbItemIndexId
+  );
 
-    const currItemIndxPos = shoppingCard.findIndex(
-    (item) => item.id === dbItemIndexId);
-
-    return (shoppingCard[currItemIndxPos] = {
-            ...shoppingCard[currItemIndxPos],
+  return (shoppingCard[currItemIndxPos] = {
+    ...shoppingCard[currItemIndxPos],
     //   added to the current value the ++ value
-        price: shoppingCard[currItemIndxPos].price + itemsDb[dbItemIndexId].price,
-         });
+    price: shoppingCard[currItemIndxPos].price + itemsDb[dbItemIndexId].price,
+  });
 }
 
-
-function delOfShoppingCard(shoppingCard, itemsDb){
-    const itemIndexId = parseInt(this.id);
-
-    const currItemIndxPos = shoppingCard.findIndex(
-        (item) => item.id === itemIndexId);
-
-        if(shoppingCard[currItemIndxPos].price > itemsDb[itemIndexId].price ) {
-            return (shoppingCard[currItemIndxPos] = {
-                ...shoppingCard[currItemIndxPos],
+function delOfShoppingCard() {
+  // id is x,01 round it with this method to whole number
+  
+  const itemIndexId = parseInt(this.id);
+  
+  const currItemIndxPos = shoppingCard.findIndex(
+    (item) => item.id === itemIndexId
+    );
+    console.log(shoppingCard[currItemIndxPos])
+    
+    
+    if (shoppingCard[currItemIndxPos].price > itemsDb[itemIndexId].price) {
+      console.log("fired")
+      return (shoppingCard[currItemIndxPos] = {
+        ...shoppingCard[currItemIndxPos],
         //   added to the current value the ++ value
-            price: shoppingCard[currItemIndxPos].price - itemsDb[itemIndexId].price,
-             });
-        } else if(shoppingCard[currItemIndxPos].price === itemsDb[itemIndexId].price) {
-
-            // splice the value
-            // shoppingCard.splice([noteArrayElementId - 1], 1) 
-
-        }
-    // find current index
-    // delete one value
-    // if value <= x
-    // delete the object
-
-    // render
-}
-
-
+        price: shoppingCard[currItemIndxPos].price - itemsDb[itemIndexId].price,
+      });
+    } else if (
+      shoppingCard[currItemIndxPos].price === itemsDb[itemIndexId].price
+      ) {
+        // Add splice to it
+        // splice the value
+        // shoppingCard.splice([noteArrayElementId - 1], 1)
+      }
+      // find current index
+      // delete one value
+      // if value <= x
+      // delete the object
+      
+      // render
+    }
